a93c6e23187dfce8a840531d219992f1160943fd
moooooora <omarmohammed271@gmail.com>
2024-04-27 19:29:57 +0300

    Added profile signal

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save,post_delete
 # Create your models here.
 
 
@@ -11,3 +13,7 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return str(self.user)
+@receiver(post_save,sender=User)
+def create_user_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)    
