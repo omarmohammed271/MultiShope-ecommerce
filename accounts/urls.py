b7ba7701c29b2a807bf82fc3f77efe760c703290
moooooora <omarmohammed271@gmail.com>
2024-05-04 19:44:53 +0300

    added forget password

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('register/',views.register,name='register'),
+    path('login/',views.login_view,name='login'),
+    path('logout/',views.logout_view,name='logout'),
+    path('forget_password/',views.forget_password,name='forget_password'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
 
@@ -12,9 +15,49 @@ def register(request):
         confirm_password = request.POST.get('password2')
         if password == confirm_password:
             user = User.objects.create_user(username=username,email=email,password=password)
+            send_mail(
+                'Succes Signup',
+                f'Your username is :{username}\n your password is: {password} \n ',
+                settings.EMAIL_HOST_USER,
+                [email],
+                fail_silently=False,
+            )
+            return redirect('accounts:login')
             
         else : 
             return HttpResponse('Invalid Password')
 
 
     return render(request,'accounts/register.html')
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username,password=password)
+        login(request,user)
+        return redirect('home')
+
+    return render(request,'accounts/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('accounts:login')
+
+def forget_password(request):
+    if request.method =="POST":
+        email = request.POST.get('email')
+    
+        new_password = User.objects.make_random_password()
+        user = User.objects.filter(email=email).first()
+        user.set_password(new_password)
+        user.save
+        send_mail(
+                'Reset Password',
+                f' your permnant password is: {new_password} \n ',
+                settings.EMAIL_HOST_USER,
+                [email],
+                fail_silently=False,
+            )
+        return redirect('accounts:reset_password')
+
+    return render(request,'accounts/forget_password.html')
